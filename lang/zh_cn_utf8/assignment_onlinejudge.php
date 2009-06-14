73cce499b23a8e6720d13c29f63e114a367b3a77
Sun Zhigang <sunner@gmail.com>
2009-06-14 08:15:29 +0000

    More accurate words

--- lang/zh_cn_utf8/assignment_onlinejudge.php
+++ lang/zh_cn_utf8/assignment_onlinejudge.php
@@ -1,9 +1,9 @@
 <?php
     $string['assignmentlangs'] = '编程语言';
-    $string['duejudge'] = '是否到截止时间后才评判';
+    $string['duejudge'] = '是否到截止时间后才评测';
     $string['errorinfo'] = '错误信息';
     $string['input'] = '输入';
-    $string['judgetime'] = '评判时间';
+    $string['judgetime'] = '评测时间';
     $string['maximumcpu'] = 'CPU最长运行时间';
     $string['maximumfilesize'] = '源文件最大长度';
     $string['maximummem'] = '内存最多可用';
@@ -11,7 +11,7 @@
     $string['statusac'] = '测试通过';
     $string['statusce'] = '编译错误';
     $string['statusie'] = '内部错误';
-    $string['statuspending'] = '等待评判...';
+    $string['statuspending'] = '等待评测...';
     $string['statusre'] = '运行时错误';
     $string['statustle'] = '运行超时';
     $string['statuswa'] = '错误答案';
