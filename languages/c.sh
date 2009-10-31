b8e22195c997bd58916fb571257ec3c966b96046
Sun Zhigang <sunner@gmail.com>
2009-10-31 23:50:13 +0000

    Define Macro _MOODLE_ONLINE_JUDGE_ for submissions

--- languages/c.sh
+++ languages/c.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# C compile wrapper-script for 'test_solution.sh'.
+# C compile wrapper-script for onlinejudge.
 # See that script for syntax and more info.
 
 SOURCE="$1"
 DEST="$2"
 
+# -D_MOODLE_ONLINE_JUDGE_:  So the submission can do differently in onlinejudge
 # -Wall:	Report all warnings
 # -O2:		Level 2 optimizations (default for speed)
 # -static:	Static link with all libraries
 # -lm:		Link with math-library (has to be last argument!)
-gcc -Wall -O2 -static -o $DEST $SOURCE -lm
+gcc -D_MOODLE_ONLINE_JUDGE_ -Wall -O2 -static -o $DEST $SOURCE -lm
 exit $?
--- languages/cpp.sh
+++ languages/cpp.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-# C++ compile wrapper-script for 'test_solution.sh'.
+# C++ compile wrapper-script for onlinejudge.
 # See that script for syntax and more info.
 
 SOURCE="$1"
 DEST="$2"
 
+# -D_MOODLE_ONLINE_JUDGE_:  So the submission can do differently in onlinejudge
 # -Wall:	Report all warnings
 # -O2:		Level 2 optimizations (default for speed)
 # -static:	Static link with all libraries
-g++ -Wall -O2 -static -o $DEST $SOURCE
+g++ -D_MOODLE_ONLINE_JUDGE_ -Wall -O2 -static -o $DEST $SOURCE
 exit $?
