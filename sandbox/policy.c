88907957bed07988412227a19b56e8c3160bdb5a
sunner <sunner@cms.hit.edu.cn>
2011-01-10 19:29:59 +0800

    Add policy 3 for suse

--- sandbox/policy.c
+++ sandbox/policy.c
@@ -49,6 +49,32 @@ int allowed_syscalls [] = {
     __NR_exit_group,
     0
 };
+#elif defined(POLICY3)
+/* policy for SUSE */
+/* syscall sequence for init code */
+int init_syscalls [] = {
+    __NR_uname,
+    __NR_brk,
+    __NR_brk,
+    __NR_set_thread_area,
+    __NR_open,
+    __NR_read,
+    __NR_close,
+    __NR_brk,
+    __NR_brk,
+    0
+};
+
+/* allowed syscalls after init */
+int allowed_syscalls [] = {
+    __NR_read,
+    __NR_write,
+    __NR_mmap2,
+    __NR_munmap,
+    __NR_fstat64,
+    __NR_exit_group,
+    0
+};
 #else
 /* policy for normal kernel. Works on Debian 5, Ubuntu */
 /* syscall sequence for init code */
