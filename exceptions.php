f7433d6ead455877b2a953682082e2223f08a4ff
Sun Zhigang <sunner@gmail.com>
2011-06-23 13:41:42 +0800

    Initial adding exceptions

---
+++ exceptions.php
@@ -0,0 +1,42 @@
+<?
+
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+//                      Online Judge for Moodle                          //
+//       https://github.com/hit-moodle/moodle-local_onlinejudge2         //
+//                                                                       //
+// Copyright (C) 2009 onwards  Sun Zhigang  http://sunner.cn             //
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 3 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+/**
+ * Online judge related exceptions
+ * 
+ * @package   local_onlinejudge2
+ * @copyright 2011 Sun Zhigang (http://sunner.cn)
+ * @author    Sun Zhigang
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+
+class onlinejudge2_exception extends moodle_exception {
+    function __construct($errorcode, $a=NULL, $debuginfo = NULL) {
+        parent::__construct($errorcode, 'local_onlinejudge2', '', $a, $debuginfo);
+    }
+}
+
+
