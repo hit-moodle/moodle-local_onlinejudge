0256fdb83bb45aad3af3eb401bc5e5550de7d3c3
Sun Zhigang <sunner@gmail.com>
2011-08-09 10:37:11 +0800

    Remove a debug branch

--- cli/judged.php
+++ cli/judged.php
@@ -117,8 +117,6 @@ while (!$forcestop and !$upgraded) {
     try {
         judge_all_unjudged();
     } catch (Exception $e) {
-        if (debugging())
-            default_exception_handler($e);
         cli_problem('Caught exception: '.$e->getMessage());
     }
 
