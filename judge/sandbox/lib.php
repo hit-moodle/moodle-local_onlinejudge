444c7c76556058e5f04444dd12cc5ac000fe58ec
Sun Zhigang <sunner@gmail.com>
2011-08-29 12:06:03 +0800

    Set -lm as the last argument for gcc
    -lm doesn't work with -static execept it is the last argument
    I don't know why, but now it works

--- judge/sandbox/lib.php
+++ judge/sandbox/lib.php
@@ -39,10 +39,10 @@ define('SANDBOX_SAND', $CFG->dirroot.'/local/onlinejudge/judge/sandbox/sand/sand
 
 class judge_sandbox extends judge_base {
     protected static $supported_languages = array(
-        'c' => 'gcc -D_MOODLE_ONLINE_JUDGE_ -Wall -static -lm -o %DEST% %SOURCES%',
-        'c_warn2err' => 'gcc -D_MOODLE_ONLINE_JUDGE_ -Wall -Werror -static -lm -o %DEST% %SOURCES%',
-        'cpp' => 'g++ -D_MOODLE_ONLINE_JUDGE_ -Wall -static -lm -o %DEST% %SOURCES%',
-        'cpp_warn2err' => 'g++ -D_MOODLE_ONLINE_JUDGE_ -Wall -Werror -static -lm -o %DEST% %SOURCES%'
+        'c' => 'gcc -D_MOODLE_ONLINE_JUDGE_ -Wall -static -o %DEST% %SOURCES% -lm',
+        'c_warn2err' => 'gcc -D_MOODLE_ONLINE_JUDGE_ -Wall -Werror -static -o %DEST% %SOURCES% -lm',
+        'cpp' => 'g++ -D_MOODLE_ONLINE_JUDGE_ -Wall -static -o %DEST% %SOURCES% -lm',
+        'cpp_warn2err' => 'g++ -D_MOODLE_ONLINE_JUDGE_ -Wall -Werror -static -o %DEST% %SOURCES% -lm'
     );
 
     static function get_languages() {
