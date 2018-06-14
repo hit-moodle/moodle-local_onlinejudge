fdccb3772caddeab21511feed88db4b5eebb3767
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2018-06-14 05:25:07 +0200

    Upgrading Client Plugin Version

--- clients/mod/assign/feedback/onlinejudge/version.php
+++ clients/mod/assign/feedback/onlinejudge/version.php
@@ -5,6 +5,9 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$plugin->version  = 2012041200;
-$plugin->requires = 2010122900;  // Requires this Moodle version
+$plugin->version = 2018061400;
+$plugin->requires = 2017111300;  // Requires this Moodle version
+$plugin->component = 'assignfeedback_onlinejudge';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = "3.0 beta4"; // User-friendly version number
 
