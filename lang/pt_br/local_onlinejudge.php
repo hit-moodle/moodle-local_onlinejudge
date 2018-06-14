57399a69226c879227e17239afcfcffcab469c7b
Andrew Nagyeb <andrewnajeeb@fci.helwan.edu.eg>
2018-06-14 04:48:46 +0200

    Upgrading Language

--- lang/pt_br/local_onlinejudge.php
+++ lang/pt_br/local_onlinejudge.php
@@ -41,40 +41,26 @@ $string['cpuusage'] 			= 'Uso da CPU';
 $string['defaultlanguage'] 		= 'Idioma padrao';
 $string['defaultlanguage_help'] = 'Definicao de idioma padrao para as novas atribuicoes do Analise Online.';
 $string['details'] 				= 'Detalhes';
-$string['ideoneautherror'] 		= 'Nome de usuario errado ou senha errada';
-$string['ideonedelay'] 			= 'Atraso entre pedidos de ideone.com (segundo)';
-$string['ideonedelay_help'] 	= 'Depois de enviar um pedido a Analise ideone.com, nao podemos obter o resultado imediatamente. Quanto tempo devemos esperar antes de consultar o resultado? 5 segundos ou entao um pouco mais.';
-$string['ideoneerror'] 			= 'Ideone retornou um erro: {$a}';
-$string['ideonelogo'] 			= '<a href="https://github.com/hit-moodle/moodle-local_onlinejudge">Analise Online de Fontes para Moodle</a> uses <a href="http://ideone.com">Ideone API</a> &copy; by <a href="http://sphere-research.com">Sphere Research Labs</a>';
-$string['ideoneresultlink'] 	= 'Veja detalhes em <a href="http://ideone.com/{$a}">http://ideone.com/{$a}</a>.';
-$string['ideoneuserrequired'] 	= 'Necessario se o Analisador ideone.com for selecionado';
+$string['seautherror'] = 'Nome de usuario errado ou senha errada';
+$string['sedelay'] = 'Atraso entre pedidos de ideone.com (segundo)';
+$string['sedelay_help'] = 'Depois de enviar um pedido a Analise ideone.com, nao podemos obter o resultado imediatamente.ï¿½Quanto tempo devemos esperar antes de consultar o resultado?ï¿½5 segundos ou entao um pouco mais.';
+$string['seerror'] = 'Ideone retornou um erro: {$a}';
+$string['selogo'] = '<a href="https://github.com/hit-moodle/moodle-local_onlinejudge">Analise Online de Fontes para Moodle</a> uses <a href="https://sphere-engine.com/">Sphere Engine Compilers API</a> &copy; by <a href="http://sphere-research.com">Sphere Research Labs</a>';
+$string['seresultlink'] = 'Veja detalhes em <a href="https://{$a->end_point}.compilers.sphere-engine.com/api/v4/submissions/{$a->submission_id}?access_token={$a->access_token}">https://{$a->end_point}.compilers.sphere-engine.com/api/v4/submissions/{$a->submission_id}?access_token={$a->access_token}</a>.';
+$string['seclientidrequired'] = 'Necessario se o Analisador ideone.com for selecionado';
 $string['info'] 				= 'Informacao';
-$string['info0'] 				= 'Se você estava esperando muito tempo, por favor informe o administrador';
-$string['info1'] 				= 'Parabens!';
-$string['info2'] 				= 'Um bom programa deve retornar 0 se nenhum erro ocorrer';
-$string['info3']				= 'O compilador de codigos nao gostou do seu';
-$string['info4']				= 'Parece que o compilador gostou de seu codigo';
-$string['info5'] 				= 'Você comeu muita memoria';
-$string['info6'] 				= 'O codigo enviado e demais para stdout';
-$string['info7'] 				= 'Quase perfeito, exceto alguns espacos em brancos, tabs, novas linhas e etc';
-$string['info8'] 				= 'Seu codigo chama algumas funcoes que <em>nao</em> sao permitidas para executar';
-$string['info9'] 				= '[SIGSEGV, falha Segmento] indice de array errado, acesso nao permitido à memoria ou pior';
-$string['info10'] 				= 'O programa tem funcionado por tempo demasiado longo';
-$string['info11'] 				= 'Confira o seu codigo. Nao adicione caracteres diferentes da linguagem';
-$string['info21'] 				= 'O motor do Analisador nao esta funcionando bem. Por favor, informe o administrador';
-$string['info22'] 				= 'Se você esta esperando por muito tempo, por favor informe o administrador';
 $string['infostudent'] 			= 'Informacoes';
 $string['infoteacher'] 			= 'Informacoes confidenciais';
 $string['invalidlanguage'] 		= 'ID de idioma invalido: {$a}';
 $string['invalidjudgeclass']	= 'Classe invalida do Analisador: {$a}';
 $string['invalidtaskid'] 		= 'Id tarefa invalido: {$a}';
 $string['judgedcrashnotify'] 	= 'A tarefa do Analisador falhou';
-$string['judgedcrashnotify_help'] = 'Se a tarefa do Analisador parar de funcionar devido a bugs de software ou atualizacoes, quem ira receber a notificacao? Deve ser uma pessoa que pode acessar o shell do servidor e iniciar a tarefa do Analisador.';
-$string['judgednotifybody'] 	= 'Entre as {$a->count} tarefas pendentes, a tarefa mais antiga ficou na fila de espera desde {$a->period}. 
+$string['judgedcrashnotify_help'] = 'Se a tarefa do Analisador parar de funcionar devido a bugs de software ou atualizacoes, quem ira receber a notificacao?ï¿½Deve ser uma pessoa que pode acessar o shell do servidor e iniciar a tarefa do Analisador.';
+$string['judgednotifybody'] 	= 'Entre as {$a->count} tarefas pendentes, a tarefa mais antiga ficou na fila de espera desde {$a->period}.ï¿½
 
 e possivel que o Analisador tenha parado de funcionar.
 
-Você deve inicia-lo o mais rapidamente possivel!';
+Vocï¿½ deve inicia-lo o mais rapidamente possivel!';
 $string['judgednotifysubject']	= '{$a->count} tarefas pendentes que esperaram tempo de mais';
 $string['judgestatus']			= 'Analisador on-line tem <strong> {$a->judged} </strong> tarefas julgadas  e <strong> {$a->pending} </strong> tarefas na fila de espera.';
 $string['langc_sandbox'] 		= 'C (executado localmente)';
@@ -98,7 +84,7 @@ $string['sandboxerror'] 		= 'Sandbox gerou um erro: {$a}';
 $string['settingsform'] 		= 'Configuracoes Analisador On-line';
 $string['settingsupdated'] 		= 'Configuracoes atualizadas.';
 $string['status0'] 				= 'Pendente ...';
-$string['status1'] 				= '<font color=red>Aceito</font>';
+$string['status1'] = '<span style="color: red; ">Aceito</span>';
 $string['status2'] 				= 'Termino anormal';
 $string['status3'] 				= 'Erro de compilacao';
 $string['status4'] 				= 'Compilacao OK';
@@ -111,9 +97,11 @@ $string['status10'] 			= 'Prazo Excedido';
 $string['status11'] 			= 'Resposta errada';
 $string['status21'] 			= 'Erro interno';
 $string['status22'] 			= 'Submeter ...';
-$string['status23']				= 'Múltiplos status';
+$string['status23']				= 'Mï¿½ltiplos status';
 $string['status255'] 			= 'Nao-Submetido';
 $string['stderr'] 				= 'Saida de erro padrao';
 $string['stdout'] 				= 'Saida padrao';
 $string['upgradenotify'] 		= 'Nao se esqueca de executar cli/install_assignment_type and cli/judged.php. Detalhes em <a href="https://github.com/hit-moodle/moodle-local_onlinejudge/blob/master/README.md" target="_blank">README</a>.';
-
+$string['event_onlinejudge_task_judged'] = "Event Online Judge Task Judged";
+$string['event_onlinejudge_task_description'] = "The event is concerned with firing the 'onlinejudge_task_judged' located in MOODLE_PATH/mod/assign/feedback/onlinejudge/lib.php.";
+// TODO: add translations to other languages.
